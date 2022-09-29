5e481600b784043c5b2f50c877a7c28739553589
jhuset2003 <jhusetqp@gmail.com>
2022-09-29 16:28:15 -0500

    Cell v1.4.3

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { SearchForm } from '../Components/Forms/SearchForm'
-import { Nav } from '../Components/Nav/Nav'
+
 import "../Styles/PagesS/LandingPage.scss"
 export const Home = () => {
   return (
     <div className='Landing'>
         <section className='home' id='Home'>
-      <section className='nav_container'>
-        <Nav/>
-      </section>
+  
 
               <div className='home_elements_container'>
                   <div className='title_container'>
